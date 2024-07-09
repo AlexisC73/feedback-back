32fb577840256d00b5974c3ef873fed0c5b6ce9b
Alexis Comte <alexis.comte73@icloud.com>
2024-07-09 18:44:28 +0200

    remove try catch from get me

--- app/controllers/auth/get_me_controller.ts
+++ app/controllers/auth/get_me_controller.ts
@@ -2,11 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class GetMeController {
   async handle({ auth, response }: HttpContext): Promise<void> {
-    try {
-      const user = await auth.authenticate()
-      return response.ok(user)
-    } catch (e) {
-      throw e
-    }
+    const user = await auth.authenticate()
+    return response.ok(user)
   }
 }
