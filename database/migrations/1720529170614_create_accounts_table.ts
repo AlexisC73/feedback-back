22f706316e4a0c7ac684d695e282f9491df35a2b
Alexis Comte <alexis.comte73@icloud.com>
2024-07-14 18:27:10 +0200

    fix create account migration

--- database/migrations/1720529170614_create_accounts_table.ts
+++ database/migrations/1720529170614_create_accounts_table.ts
@@ -9,7 +9,7 @@ export default class extends BaseSchema {
       table.uuid('id', { primaryKey: true })
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-      table.string('role').defaultTo(Role.USER).notNullable()
+      table.integer('role').defaultTo(Role.USER).notNullable()
       table.string('avatar').nullable()
       table.string('display_name').notNullable()
       table.string('username').notNullable().unique()
